fcd79f45253e2a4345f4a0820e1365fdb357fe4b
gitAji <89026824+gitAji@users.noreply.github.com>
2022-01-10 20:29:17 +0100

    Done!

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// question 1
+var age=50;
+var subject="JavaScript";
+var overAge=true;
+// declaring variables Number,string,boolean 
+console.log("MA1-semester2");
+// question 2
+const firstName="Ajeerathan";
+const lastName="Kumarasamy";
+// console.log("My name is"+" "+firstName+" "+lastName);
+const fullName=(firstName+" "+lastName);
+console.log(fullName);
+// question 3
+console.log(typeof "frog");
+// print the type of value for frog
+const type="string";
+console.log("The type of frog is"+" " +type);
+// question 4
+var orderHasShipped=false;
+
+if(orderHasShipped===true){
+    console.log("The order shipped");
+}
+else{
+    console.log("The order is not shipped");
+}
+// consol log order is not shipped
+
+//question 5
+
+for (var input = 7; input <= 13; input++) {
+    console.log(input);
+   }
